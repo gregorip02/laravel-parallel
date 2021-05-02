e98ccf9c92d77e0d82af92893a62814371b96633
gregorip02 <gregorip02@users.noreply.github.com>
2021-05-02 21:55:09 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,7 @@
 
 namespace Stubleapp\Parallel\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Gregorip02\LaravelParallel\LaravelParallelServiceProvider;
 
 class TestCase extends Orchestra
 {
