17f8c8cf13f2953f86034fc2d46c0db2f1304c83
Gregori Piñeres <gregori.pineres02@gmail.com>
2021-05-15 18:50:44 -0400

    Remove rule "trailing_comma_in_multiline_array"

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -14,7 +14,6 @@ return $fixer->setFinder($finder)->setRules([
     'ordered_imports' => ['sortAlgorithm' => 'alpha'],
     'no_unused_imports' => true,
     'not_operator_with_successor_space' => true,
-    'trailing_comma_in_multiline_array' => true,
     'phpdoc_scalar' => true,
     'unary_operator_spaces' => true,
     'binary_operator_spaces' => true,
